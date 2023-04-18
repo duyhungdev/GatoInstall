75fcea3dfdf741113926c7a482cbf6afce0ca6cb
duyhungdev <131138773+duyhungdev@users.noreply.github.com>
2023-04-18 14:40:35 +0700

    Add files via upload

---
+++ Install client.bat
@@ -0,0 +1,33 @@
+@echo off
+color b
+title Installing...
+echo -
+echo Installing GatoClient...
+curl -s -k -L -O https://github.com/Gatohost/gatoclient/releases/download/v1.1.7/Gatoclient-Remastered-portable-win.exe
+move Gatoclient-Remastered-portable-win.exe GatoClient.exe
+mkdir Gatoclient-Remastered
+cd Gatoclient-Remastered
+echo -
+echo OK!
+echo Installing Config...
+curl -s -k -L -O https://raw.githubusercontent.com/duyhungdev/GatoInstall/main/Gatoclient-Remastered/config.json
+echo OK!
+echo -
+mkdir addons
+cd addons
+echo Installing addons...
+curl -s -k -L -O https://raw.githubusercontent.com/duyhungdev/GatoInstall/main/Gatoclient-Remastered/addons/KeyBoard-Overlay.js
+curl -s -k -L -O https://raw.githubusercontent.com/duyhungdev/GatoInstall/main/Gatoclient-Remastered/addons/altManager.js
+echo OK!
+echo -
+cd ..
+cd ..
+echo Installing ResourceSwapper...
+curl -s -k -L -O https://github.com/duyhungdev/GatoInstall/raw/main/GatoclientResourceSwapper.zip
+tar -xf GatoclientResourceSwapper.zip
+del GatoclientResourceSwapper.zip
+echo OK!
+echo -
+echo "cai dat thanh cong!"
+move GatoclientResourceSwapper %USERPROFILE%
+move Gatoclient-Remastered %username%/Documents
